0ddee651ce7322df04b19c1cf3b76c18e86bd46b
hasanujjamanjibon <hasanujjamanjibon2@gmail.com>
2022-07-01 12:31:23 +0600

    updated

---
+++ jquery/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+  var options = {
+    strings: ["Hasan Ujjaman Jibon.", "Digital Marketing Expert."],
+    typeSpeed: 40,
+    backSpeed: 40,
+    showCursor: false,
+    loop: true,
+  };
+
+  var typed = new Typed(".typing", options);
+
+  $(".skill-per").each(function () {
+    var $this = $(this);
+    var per = $this.attr("per");
+    $this.css("width", per + "%");
+    $({ animatedValue: 0 }).animate(
+      { animatedValue: per },
+      {
+        duration: 1000,
+        step: function () {
+          $this.attr("per", Math.floor(this.animatedValue) + "%");
+        },
+        complete: function () {
+          $this.attr("per", Math.floor(this.animatedValue) + "%");
+        },
+      }
+    );
+  });
+  $(".js--about-section").waypoint(function (direction) {
+    if (direction == "down") {
+      $("nav").addClass("sticky");
+    } else {
+      $("nav").removeClass("sticky");
+    }
+  });
+});
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
